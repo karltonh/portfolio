5dfab3ae1a7158df06de88b4496d05be4987f2fc
karltonh <karltonhall713@gmail.com>
2024-01-25 16:20:27 -0600

    updated About page with more information.

--- src/About.js
+++ src/About.js
@@ -4,6 +4,9 @@ import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import ButtonBase from '@mui/material/ButtonBase';
 import photo from "./images/IMG_0932.jpg";
+import { Link } from 'react-router-dom';
+
+
 const Img = styled('img')({
     margin: 'auto',
     display: 'block',
@@ -14,16 +17,24 @@ function About() {
     return (
         <div >
         <Grid container spacing={{ xs: 2, md: 8}} columns={{ xs: 4, sm: 4, md: 8 }}>
-        <Grid item class='app' xs= {2} style={{marginTop: "7%", marginLeft: "10%", width: "80%",height: "80%"}}>
-                    <Paper square={false} style={{height:"100%"}}>
-                    <ButtonBase sx={{ width: "20%", height: "90%" }} style={{marginRight: '10%'}}>
-                    <Img alt="complex" src={photo} style={{marginTop: '10%', marginLeft: "10%", width: "100%", height: "100%"}}/>     
+            <Grid item class='app' xs= {2} style={{marginTop: "7%", marginLeft: "10%", width: "80%",height: "80%"}}>
+                <Paper square={false} style={{height:"100%", display: 'flex'}}>
+                    <ButtonBase sx={{ width: "20%", height: "90%" }} style={{marginRight: '10%', flexDirection: 'row'}}>
+                        <Img alt="complex" src={photo} style={{marginTop: '5%', marginLeft: "10%", marginBottom: '5%', width: "100%", height: "90%"}}/>     
                     </ButtonBase>
-                    <ButtonBase sx={{ width: 128, height: 128 }}>     
-                    COMING SOON
+                    <ButtonBase sx={{ width: '25%', height: '10%' }} style={{flexDirection: 'column'}}>   
+                        <a class="paneltitle">ABOUT ME: </a>
+                        <a class="panelContent">I am a software developer currently specializing in game development.</a>   
+                        <a class="panelContent">{"I graduated from Texas Tech with B.S. in Computer Science, my senior capstone project was to fine tune an LLM (we chose Falcon 7b from hugging face) to meet specifications given to us by Lockheed Martin"}</a>
+                        <Link to={"https://karltonh.github.io/Rollingthunder_ttu/"}><a class="panelContent">(More information on capstone project)</a></Link>
+                        <a class="panelContent">This website serves as a public place for my portfolio.</a>
                     </ButtonBase>
-                  </Paper>
-                </Grid>
+                    <ButtonBase sx={{ width: '25%', height: '10%'}} style={{flexDirection: 'column'}}>   
+                        <a class="paneltitle">CURRENT PROJECTS: </a>
+                        <a class="panelContent">Space Game.</a>    
+                    </ButtonBase>
+                </Paper>
+            </Grid>
         </Grid>
       </div>
         );
--- src/Home.js
+++ src/Home.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
+//import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import { Route, Link , Routes} from 'react-router-dom';
@@ -8,15 +8,17 @@ import ButtonBase from '@mui/material/ButtonBase';
 import BlackSkyBox from "./assets/02-34-11-741_512.gif";
 import DevlogImage from "./images/devlogupdate.jpg";
 
+
 const Img = styled('img')({
   margin: 'auto',
   display: 'block',
   maxWidth: '100%',
   maxHeight: '100%',
 });
+
+
 function Home() {
     const [date, setDate] = useState(new Date());
-    const [temp, setTemp] = useState();
     function refreshClock() {
       setDate(new Date());
 
@@ -27,13 +29,13 @@ function Home() {
       event.target.style.color = 'gray';
     }
     function MouseOverPanel(event){
-      if(event.target.style.bg=="t"){
+      if(event.target.style.bg==="t"){
         event.target.style.width = "100%";
         event.target.style.height="150%";
       }
     }
     function MouseOffPanel(event){
-      if(event.target.style.bg=="t"){
+      if(event.target.style.bg==="t"){
         event.target.style.width = "80%";
         event.target.style.height="120%";
       }
@@ -63,23 +65,21 @@ function Home() {
 
         return  <Grid item class='app' xs= {2} style={{marginTop: "7%", marginLeft: "5%", width: "20%"}}>
                   <Link to={link} style={{textDecoration: "none"}}>
-                  <Paper square={false} style={{height:"120%", width: "80%", bg:"t"}} onMouseOver={MouseOverPanel} onMouseOut={MouseOffPanel}>
-                    <Item><li class="griditem">{text}</li></Item>
-                    <ButtonBase sx={{ width: 128, height: 128 }} style={{marginRight: '10%'}}>     
+                    <Paper variant="contained" square={false} style={{height:"120%", width: "80%", bg:"t"}} onMouseOver={MouseOverPanel} onMouseOut={MouseOffPanel}>
+                      <Item><li class="griditem" onMouseOver={MouseOverLink}>{text}</li></Item>
+                      <ButtonBase sx={{ width: 128, height: 128 }} style={{marginRight: '10%'}}>     
                         <Img alt="complex" src={image} style={{marginTop: '10%', marginLeft: "10%", pointerEvents: 'none'}}/>
-                    </ButtonBase>
-                    <ButtonBase sx={{ width: 128, height: 128 }}>     
+                      </ButtonBase>
+                      <ButtonBase sx={{ width: 128, height: 128 }}>     
                         {body}
-                    </ButtonBase>
-                  </Paper>
+                      </ButtonBase>
+                    </Paper>
                   </Link>
                 </Grid>
         }
     return (
         <div>
-            <div class="clock">
-                {date.toLocaleTimeString()}
-            </div>
+            <div class="clock">{date.toLocaleTimeString()}</div>
             <div class="homeitems" style={{marginLeft:"3%"}} >
               <Grid container spacing={{ xs: 2, md: 8}} columns={{ xs: 4, sm: 4, md: 8 }}>
                 {renderHeader("SPACE GAME (Gamejam)", "/space", "A project for the scripto game jam using babylonjs (early stages)", BlackSkyBox)}
